ac8c657743c6473745d97af3f4a9723b1c5bcf60
eiu165 <eiu165@gmail.com>
2012-01-09 13:02:03 -0600

    adding braintree sample to code

--- Source/Web/Controllers/HomeController.cs
+++ Source/Web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Braintree;
+using System.Text;
 
 namespace MvcMovie.Controllers
 {
@@ -32,8 +34,61 @@ namespace MvcMovie.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public virtual ActionResult PersonalInfo(int id, FormCollection collection)
+        public virtual ActionResult PersonalInfo(FormCollection collection)
         {
+            var a = 9;
+
+            var gateway = new BraintreeGateway
+            {
+                Environment = Braintree.Environment.SANDBOX,
+                MerchantId = "the_merchant_id",
+                PublicKey = "a_public_key",
+                PrivateKey = "a_private_key"
+            };
+
+            var request = new TransactionRequest
+            {
+                Amount = 1000M,
+                CreditCard = new TransactionCreditCardRequest
+                {
+                    Number = collection["Card Number"],       // "4111111111111111",
+                    ExpirationDate = collection["Exp Date"]   // "05/2012"
+                }
+            };
+
+            Result<Transaction> result = gateway.Transaction.Sale(request);
+
+            if (result.IsSuccess())
+            {
+                Transaction transaction = result.Target;
+                this.FlashInfo("Success!: " + transaction.Id);
+            }
+            else if (result.Transaction != null)
+            {
+                var sb = new StringBuilder();
+                sb.AppendFormat("Message:  {0}", result.Message);
+                Transaction transaction = result.Transaction;
+                sb.AppendFormat("Error processing transaction:");
+                sb.AppendFormat("  Status:  {0}", transaction.Status);
+                sb.AppendFormat("  Code:  {0}", transaction.ProcessorResponseCode);
+                sb.AppendFormat("  Text:  {0}", transaction.ProcessorResponseText);
+
+            }
+            else
+            {
+                var sb = new StringBuilder();
+                sb.AppendFormat("Message: {0}", result.Message);
+                foreach (ValidationError error in result.Errors.DeepAll())
+                {
+                    sb.AppendFormat("Attribute: {0}", error.Attribute);
+                    sb.AppendFormat("  Code: {0}", error.Code);
+                    sb.AppendFormat("  Message: {0}", error.Message);
+                }
+                this.FlashInfo(sb.ToString());
+            }
+
+
+
             return View( );
             //var model = _table.CreateFrom(collection);
             //try
