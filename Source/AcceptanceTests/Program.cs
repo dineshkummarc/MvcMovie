5f6e867fd719a7156a8fbe5115d459f7e4ea5efd
eiu165 <eiu165@gmail.com>
2012-02-16 12:17:10 -0600

    refactor program

--- Source/AcceptanceTests/Program.cs
+++ Source/AcceptanceTests/Program.cs
@@ -5,27 +5,44 @@ using System.Text;
 using Quixote;
 
 
-namespace Vidpub.AcceptanceTests {
-    class Program {
-        static void Main(string[] args) {
-            Runner.SiteRoot = "http://localhost:61100/";
-            TheFollowing.Describes("Home Page");
+namespace Vidpub.AcceptanceTests 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner.SiteRoot = "http://nmacellaio7:17222"; 
+            HomePage(); 
+            LoginPage(); 
+            Console.Read();
+        }
 
-            It.Should("Have 'MVC Movie App' in the title", () =>
+
+        static void HomePage()
+        {
+            TheFollowing.Describes("Home Page"); 
+            It.Should("Have 'Index' in the title", () =>
             {
                 return Runner.Get("/").Title.ShouldContain("Index");
             });
+            It.Should("Have 'Index' in the body", () =>
+            {
+                return Runner.Get("/").Body.ShouldContain("Index");
+            });
+        }
 
+
+        static void LoginPage()
+        {
+            TheFollowing.Describes("LoginPage");
             It.Should("fail Log in with incorrect credentials", () =>
             {
-                var post = Runner.Post("/account/logon", new  { UserName = "a", Password = "a" });
+                var post = Runner.Post("/account/logon", new { UserName = "a", Password = "a" });
                 //Console.WriteLine(post.Body);
                 //return post.Body.ShouldContain("Welcome");
                 return post.Body.ShouldContain("Login was unsuccessful");
             });
-             
 
-            Console.Read();
         }
     }
 }
