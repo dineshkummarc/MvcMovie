889b3feeed7ace18eb9ae1b9ac18efdc7ea2b01f
eiu165 <eiu165@gmail.com>
2012-02-24 12:43:20 -0600

    added api

---
+++ Source/Web/Areas/Ranch/RanchAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MvcMovie.Areas.Ranch
+{
+    public class RanchAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Ranch";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Ranch_default",
+                "Ranch/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
