e8f6449c6e8d42b17305d7ed3c392197b30f30d2
unknown <nmacellaio@.(none)>
2012-04-02 20:50:01 -0500

    move logdto

--- Source/Web/Areas/Admin/Controllers/LogsController.cs
+++ Source/Web/Areas/Admin/Controllers/LogsController.cs
@@ -17,19 +17,6 @@ using System.Linq;
 namespace MvcMovie.Areas.Admin.Controllers
 {
 
-    public class LogDto
-    {
-        public int Id { get; set; }
-        public string Summary {get; set;} 
-        public string Level {get; set;} 
-        public string IpAddress {get; set;} 
-        public string Server {get; set;} 
-        public string Session {get; set;} 
-        public string UserName {get; set;} 
-        public string Email {get; set;} 
-        public DateTime UpdatedAt {get; set;}  
-    }
-
     [AuthorizeByRole(Roles = "Administrator,Dev,Log,Audit")]  
     public class LogsController : CruddyController
     {
--- Source/Web/Areas/Admin/Models/Log.cs
+++ Source/Web/Areas/Admin/Models/Log.cs
@@ -9,11 +9,19 @@ namespace MvcMovie.Models
         }
     }
 
-    public class LogModel
+    public class LogDto
     {
-        
-
+        public int Id { get; set; }
+        public string Summary { get; set; }
+        public string Level { get; set; }
+        public string IpAddress { get; set; }
+        public string Server { get; set; }
+        public string Session { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public System.DateTime UpdatedAt { get; set; }
     }
+
 
 
 }
